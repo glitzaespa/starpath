809f86ca68a569df9a6480780ea1a084d60ace10
glitzaespa <101361608+glitzaespa@users.noreply.github.com>
2022-11-17 16:36:50 -0300

    Create pool.js

---
+++ pool.js
@@ -0,0 +1,14 @@
+{
+"classy": [
+  {
+    "pool_member":"MH;PB;YC;KR;WJ;KS;HH",
+    "pool_normal":"CLST"
+  }
+  ],
+  "ive": [
+  {
+    "pool_member":"JWY;AYJ;LE;GA;RE;LI",
+    "pool_normal":"IVNA"
+  }
+  ]
+}
